c4b3e0d6686b3533cb5eca745c422650851324d9
erikjskie <erikjskie@users.noreply.github.com>
2020-01-13 05:35:44 -0600

    moved old landing page to svg based responsive page

--- app.js
+++ app.js
@@ -1,26 +1,47 @@
-const navSlide = () => {
-  const burger = document.querySelector('.burger');
-  const nav = document.querySelector('.nav_links');
-  const navLinks = document.querySelectorAll('.nav_links li');
+// const logo =document.querySelectorAll("#desktop-background path");
 
-  burger.addEventListener('click', () => {
-    //toggle nav
-    nav.classList.toggle('nav-active');
-    //animate links
-    navLinks.forEach((link, index) => {
-      if(link.style.animation){
-        link.style.animation = '';
-      } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${(index / 3) + 0.4}s`;
-      }
-    });
-    //burger animation
-    burger.classList.toggle('toggle');
-  });
+// const logoLines = document.querySelectorAll("#desktop-background line");
+
+// for(let i = 0; i < logo.length; i++){
+//   console.log(`strand ${i} is ${logo[i].getTotalLength()}`);
+// }
+
+// for(let i = 0; i < logoLines.length; i++){
+//   console.log(`line ${i} is ${logoLines[i].getTotalLength()}`);
+// }
+
+console.log(document.getElementById("desktop-background").getBoundingClientRect());
+
+//Global Variables
+let svgBaseWidth = 1298;
+let svgBaseHeight = 991;
+let background = document.getElementById("desktop-background").getBoundingClientRect();
+let svgScaleWidth = background.width/svgBaseWidth;
+let svgScaleHeight = background.height/svgBaseHeight;
+
+function rescaleElement(elementId, top, left){
+  let element = document.getElementById(elementId);
+  const abs_top = background.top + window.scrollY + top*svgScaleHeight;
+  const abs_left = background.left + window.scrollX + left*svgScaleWidth;
+  element.style.top = abs_top.toString()+'px';
+  element.style.left = abs_left.toString()+'px';
 }
 
-const app = () => {
-  navSlide();
+function rescaleUI(){ //Warning!! This is going to get hacky
+  rescaleElement("projects", 200, 180);
+  rescaleElement("resume", 200, 1115);
+  rescaleElement("research", 800, 180);
+  rescaleElement("contacts", 800, 1115);
 }
 
-app();
+window.onresize = function(){
+  //location.reload();
+  svgBaseWidth = 1298;
+  svgBaseHeight = 991;
+  background = document.getElementById("desktop-background").getBoundingClientRect();
+  svgScaleWidth = background.width/svgBaseWidth;
+  svgScaleHeight = background.height/svgBaseHeight;
+  rescaleUI();
+};
+
+rescaleUI();
